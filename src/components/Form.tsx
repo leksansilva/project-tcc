e1d0ea34dc3115456a4849d6ab57ec928074047f
leksansilva <lek.2000@outlook.com>
2023-06-21 20:33:31 -0300

    :recycle: modfied description form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -14,7 +14,7 @@ export default function Form({ onSubmit, loading }: FormProps) {
             Coleta de dados
           </h2>
           <p className="mt-1 text-sm leading-6 text-gray-600">
-            Joga os dados aí Paulete
+            Insira as informações solicitadas
           </p>
 
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
